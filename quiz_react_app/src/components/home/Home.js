e9c60285df623b09f369886b2d33c5d23b2ff52c
Manh Phan <imseirousmanh@gmail.com>
2025-06-28 20:43:16 +0700

    add video and design to fit full-screen video

--- quiz_react_app/src/components/home/Home.js
+++ quiz_react_app/src/components/home/Home.js
@@ -1,6 +1,20 @@
-const Home =(props) =>{
+import VideoHomePage from '../../assets/video-homepage.mp4';
+
+const Home = (props) => {
     return (
-        <div className="homepage-container">Home component</div>
-    )
-}
+        <div className="homepage-container">
+            
+            <video
+                
+                
+                autoPlay
+                loop
+                muted
+            >
+                <source type="video/mp4" src={VideoHomePage} />
+            </video>
+        </div>
+    );
+};
+
 export default Home;
