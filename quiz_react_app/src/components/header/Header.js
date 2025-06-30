3f006a07c66ff652f7e9ad5429b7eb65b2381bb0
Manh Phan <imseirousmanh@gmail.com>
2025-06-30 10:43:57 +0700

    design homepage with responsive

--- quiz_react_app/src/components/header/Header.js
+++ quiz_react_app/src/components/header/Header.js
@@ -22,17 +22,15 @@ const Header =() => {
             <Nav.Link href="/admins">Admin</Nav.Link> */}
           </Nav>
           <Nav>
-          <NavDropdown title="Setting" id="basic-nav-dropdown">
+            <button className='btn-login'>Log in</button>
+            <button className='btn-signup'>Sign up</button>
+          {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item >Log in</NavDropdown.Item>
               <NavDropdown.Item >
                 Log out
               </NavDropdown.Item>
               <NavDropdown.Item >Profile</NavDropdown.Item>
-              {/* <NavDropdown.Divider /> */}
-              {/* <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item> */}
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- quiz_react_app/src/components/home/Home.js
+++ quiz_react_app/src/components/home/Home.js
@@ -1,20 +1,32 @@
-import VideoHomePage from '../../assets/video-homepage.mp4';
-
+import VideoHomePage from "../../assets/video-homepage.mp4";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 const Home = (props) => {
-    return (
-        <div className="homepage-container">
-            
-            <video
-                
-                
-                autoPlay
-                loop
-                muted
-            >
-                <source type="video/mp4" src={VideoHomePage} />
-            </video>
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source type="video/mp4" src={VideoHomePage} />
+      </video>
+
+      <div className="homepage-content">
+        <div className="title-1">There're better way to ask</div>
+        <div className="title-2 title-highlight">You don't want to make a boring form. And your audiences won't answer one. Create a typeform instead and make everyone happy.</div>
+        <div className="title-3">
+          <button className="btn btn-dark">Get's started. It's free.</button>
         </div>
-    );
+        <div className="content-notice">
+          <div className="notice-1">
+            <FontAwesomeIcon icon={faCheck} className="icon" />
+            No credit card required
+          </div>
+          <div className="notice-2">
+            <FontAwesomeIcon icon={faCheck} className="icon" />
+            No time limit on Free plan
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
