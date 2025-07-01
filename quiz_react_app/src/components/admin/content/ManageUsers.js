d0344093613788a218961aa9eed7dc0951a8268a
Manh Phan <imseirousmanh@gmail.com>
2025-07-01 16:51:34 +0700

    complere Previewing Image and add states for User

--- quiz_react_app/src/components/admin/content/ManageUsers.js
+++ quiz_react_app/src/components/admin/content/ManageUsers.js
@@ -1,14 +1,14 @@
 import CreateUserModal from "./modals/users/CreateUserModal";
-
+import "./ManageUser.scss";
 const ManageUsers = (props) => {
   return (
     <div className="manage-users-container">
       <div className="title">Manage User</div>
       <div className="users-content">
-        <button> Add new user</button>
-      </div>
-      <div>
-        table users
+        <div>
+          <button> Add new user</button>
+        </div>
+        <div>table users</div>
         <CreateUserModal />
       </div>
     </div>
--- quiz_react_app/src/components/admin/content/modals/users/CreateUserModal.js
+++ quiz_react_app/src/components/admin/content/modals/users/CreateUserModal.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { LuImagePlus } from "react-icons/lu";
 
 function CreateUserModal() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewAvatar, setPreviewAvatar] = useState("");
+
+  const handleUploadImage = (event) => {
+    if (event.target && event.target.files && event.target.files[0]) {
+      setPreviewAvatar(URL.createObjectURL(event.target.files[0]));
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
 
   return (
     <>
@@ -22,29 +37,58 @@ function CreateUserModal() {
           <form className="row g-3">
             <div className="col-md-6">
               <label className="form-label">Email</label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Fullname</label>
-              <input type="text" className="form-control" placeholder="User's fullname" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="User's fullname"
+                value={fullName}
+                onChange={(event) => {
+                  setFullName(event.target.value);
+                }}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Role</label>
-              <select className="form-select">
-                <option selected value={"USER"}>
-                  USER
-                </option>
+              <select
+                className="form-select"
+                onChange={(event) => {
+                  setRole(event.target.value);
+                }}
+              >
+                <option value={"USER"}>USER</option>
                 <option value={"ADMIN"}>ADMIN</option>
               </select>
             </div>
-            <div className="col-12">
-              <label className="form-label">Avatar</label>
-              <input type="file" className="form-control" placeholder="Apartment, studio, or floor" />
+            <div className="col-12 label-uploading ">
+              <label className="form-label btn " htmlFor="lableUploading">
+                <LuImagePlus /> Avatar
+              </label>
+              <input type="file" className="form-control" id="lableUploading" hidden onChange={(event) => handleUploadImage(event)} />
             </div>
+
+            <div className="col-md-12 modal-add-user">{!previewAvatar ? <span>Preview Image</span> : <img src={previewAvatar} alt="User avatar preview" className="img-preview"></img>}</div>
           </form>
         </Modal.Body>
         <Modal.Footer>
